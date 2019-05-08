dfd6163e8c61565093b3c655b8bfabc07b665c79
Frankie <franklin18ru>
2019-05-08 15:01:53 +0000

    temp commit

--- Indigo/urls.py
+++ Indigo/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('frontpage.urls')),
-    path('admin/', include('admin.urls')),
-    path('user/', include('user.urls')),
-    path('property/', include('property.urls')),
+    path('admins/', include('admin.urls')),
+    path('users/', include('user.urls')),
+    path('properties/', include('property.urls')),
 ]
