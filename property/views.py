acb74da519c5d3c6c4363c7ce85ddb41533f19a0
Franklin Vale <franklin18@ru.is>
2019-05-08 21:50:41 +0000

    minor changes

--- property/views.py
+++ property/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from property.models import Properties
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'property/index.html')
+    context = {'properties': Properties.objects.all().order_by('streetName')}
+    return render(request, 'property/index.html', context)
 
 def property(request):
     return render(request, 'property/property.html')
