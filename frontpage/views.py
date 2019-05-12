a9eab83957c908907c7977a21fcf057371cf05f8
olafur98 <olafurb18@ru.is>
2019-05-12 14:51:44 +0000

    Login,logout and registration functionality. Forgot password functionality.

--- frontpage/views.py
+++ frontpage/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
-
 # Create your views here.
 
-def index(request):
-    return render(request, 'frontpage/index.html')
+def index(request, user=None):
+    return render(request, 'frontpage/index.html', {'user': request.user})
