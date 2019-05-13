ac38a2e766a7b773b03e66aafe544adc8c3c7aa5
mariamjoll1 <mariahrafns@gmail.com>
2019-05-13 15:26:44 +0000

    Fixied styling for dropdown, so it works

---
+++ static/js/front.js
@@ -0,0 +1,12 @@
+$("ul").click(function(event){
+  event.stopPropagation();
+});
+
+var toggler = document.getElementsByClassName("toggle");
+var i;
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
