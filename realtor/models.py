7c05993ed30b458d1d476c39c5cc4ea6f1e9bc20
olafur98 <olafurb18@ru.is>
2019-05-14 22:18:29 +0000

    fix

--- realtor/models.py
+++ realtor/models.py
@@ -3,19 +3,15 @@ from user.models import Users
 
 # Create your models here.
 
+class Positions(models.Model):
+    name = models.CharField(max_length=255)
+
 class Realtors(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     description = models.CharField(max_length=999)
     password = models.CharField(max_length=255)
 
-class Positions(models.Model):
-    name = models.CharField(max_length=255)
-
-
-class RealtorPosition(models.Model):
-    realtor = models.ForeignKey(Realtors, on_delete=models.CASCADE)
-    position = models.ForeignKey(Positions, on_delete=models.CASCADE)
 
 
 class SoldProperty(models.Model):
