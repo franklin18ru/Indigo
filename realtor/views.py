f3ecca1f0e2de3c22755b8adc87e33baf4519edf
margreta <margreta11@ru.is>
2019-05-15 13:41:35 +0000

    fixing database connection to realtor.positions, along with other detailing on footer and its function.

--- realtor/views.py
+++ realtor/views.py
@@ -3,9 +3,11 @@ from property.models import Properties
 
 # Create your views here.
 
+
 def index(request):
     context = {'properties': Properties.objects.all().order_by('streetName')}
     return render(request, 'realtor/realtor.html', context)
+
 
 def getPropertyByRealtorId(request, id):
     context = {'property': get_object_or_404(Properties, pk=id)}
